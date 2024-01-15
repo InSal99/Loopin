c7374ab75154affdf377fbf1bd4351b1a3425259
InSal99 <85535998+InSal99@users.noreply.github.com>
2024-01-15 14:33:06 +0700

    add tutorial view model

---
+++ Loopin/Loopin/Features/Models/Term.swift
@@ -0,0 +1,8 @@
+//
+//  Term.swift
+//  Loopin
+//
+//  Created by Intan Saliya Utomo on 15/01/24.
+//
+
+import Foundation
---
+++ Loopin/Loopin/Features/ViewModels/TermViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TermViewModel.swift
+//  Loopin
+//
+//  Created by Intan Saliya Utomo on 15/01/24.
+//
+
+import Foundation
